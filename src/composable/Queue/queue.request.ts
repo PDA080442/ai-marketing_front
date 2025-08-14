273bb39024df1899379baa9f05b112e77a78395f
PDA080442 <pda080442@mail.ru>
2025-08-15 03:13:29 +0700

    feat: cancel btn without back

--- src/composable/Queue/queue.request.ts
+++ src/composable/Queue/queue.request.ts
@@ -7,3 +7,7 @@ export async function getQueueLinks(token: string): Promise<QueueItems[]> {
   const response = (await call(`/queue?token=${token}`, {}, 'GET')) as QueueResponse
   return response.items
 }
+
+export async function cancelQueueLink(id: number, token: string): Promise<void> {
+  await call(`/queue/${id}/?token=${token}`, {}, 'DELETE')
+}
