9ab9e998817126316045ff728fcefa62c3135d46
PDA080442 <pda080442@mail.ru>
2025-08-19 13:58:08 +0700

    fix: extend type

--- src/types/Gallery/gallery.types.ts
+++ src/types/Gallery/gallery.types.ts
@@ -1,15 +1,18 @@
-export interface ScreenshotItem {
-  id: string
+export interface ScreenshotItem extends BaseGallery {
   url: string
   alt: string
   width: number
   height: number
   pageTitle?: string
-  createdAt?: string
 }
 
 export interface ScreenshotDownload {
   id: string
   url: string
   fileName: string
+}
+
+export interface BaseGallery {
+  id: string
+  createdAt?: string
 }
